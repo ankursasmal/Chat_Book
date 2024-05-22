c2e2ab88be1f63cdadb7afbdba2e236dc0c1caeb
ANKUR SASMAL <ankursasmal@ANKURs-MacBook-Air.local>
2024-05-22 20:07:39 +0530

    complete

---
+++ src/components/Slider1.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Slider1() {
+  return (
+    <div>
+       <div className='sliderborder flex flex-col   mx-2 w-[205px] border-black border-[.5px] rounded-md !importent      md:w-[300px]  lg:w-[300px] h-[420px] '  >
+  
+  <img className='h-[100%] w-[100%]  '  src="https://img.freepik.com/free-photo/young-bearded-man-with-striped-shirt_273609-5677.jpg"  alt=""  />
+   
+<div className=' flex static flex-col mt-[-24%] ml-[4%] w-[70px]  h-[30px] text-white  overtext '>
+<h4 className='text-black' >Arita</h4>
+<h4 className=' text-black mt-[-6%]'>parai</h4>
+</div>
+
+     </div>
+    </div>
+  )
+}
+
+export default Slider1
--- src/components/SliderMain.jsx
+++ src/components/SliderMain.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import Slider from './Slider'
+ 
 import SliderStart from './SliderStart'
 import style from './SliderMain.module.css'
+import Slider1 from './Slider1'
 function SliderMain() {
   return (
     <div  >
       <div  className={`flex mt-[87px] md:mt-2 ${style.hideslider}   overflow-x-auto ! importent     bg-[#F2F3F5] `}>
         <SliderStart />
-<Slider/> 
+        <Slider1/>
+ <Slider/> 
 <Slider/>
 <Slider/>
 <Slider/>
